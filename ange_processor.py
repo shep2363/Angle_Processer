dd69413e4ab1ce03200c0369fb5931d47426908f
shep2363 <117785624+shep2363@users.noreply.github.com>
2023-08-17 06:07:13 -0600

    F-Version

--- ange_processor.py
+++ ange_processor.py
@@ -1,80 +1,83 @@
+import os
 import re
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-last_processed = {}
+modified_files = set()
 
-# Function to modify .idstv files
-def modify_idstv(filepath):
-    with open(filepath, 'r') as file:
-        lines = file.readlines()
-
-    if len(lines) > 10 and "<ProfileType>L</ProfileType>" in lines[9]:
-        length_pattern = re.compile(r'<Length>(\d+)</Length>')
-        tags_to_modify = ['Filename', 'DrawingIdentification', 'PieceIdentification']
-
-        for index, line in enumerate(lines):
-            length_match = length_pattern.search(line)
-            if length_match and int(length_match.group(1)) < 279:
-                for tag in tags_to_modify:
-                    start_tag = f"<{tag}>"
-                    end_tag = f"</{tag}>"
-                    if start_tag in line and end_tag in line:
-                        start = line.find(start_tag) + len(start_tag)
-                        end = line.find(end_tag)
-                        modified_value = line[start:end].replace('0', '')
-                        lines[index] = line[:start] + modified_value + line[end:]
-        
-        time.sleep(0.1)  # Delay before writing back to the file
-
-        with open(filepath, 'w') as file:
-            file.writelines(lines)
-
-# Function to modify .nc1 files
-def modify_nc1(filepath):
-    with open(filepath, 'r') as file:
-        lines = file.readlines()
-
-    if len(lines) > 10:
+def safe_file_read(filepath, retries=5, delay=0.2):
+    """Safely read a file with retry mechanism."""
+    for _ in range(retries):
         try:
-            length_value = float(lines[10].strip())
-        except ValueError:
-            return
-        
-        if length_value < 279:
-            for i in [3, 4]:
-                lines[i] = lines[i].replace('0', '')
+            with open(filepath, 'r') as file:
+                return file.readlines()
+        except Exception as e:
+            print(f"Error reading file {filepath}. Retrying... Error: {e}")
+            time.sleep(delay)
+    print(f"Failed to read {filepath} after multiple attempts.")
+    return None
 
-        time.sleep(0.1)  # Delay before writing back to the file
+def modify_idstv(filepath):
+    lines = safe_file_read(filepath)
+    if lines is None:
+        return
+
+    content = ''.join(lines)
+    if re.search(r'<ProfileType>L</ProfileType>.*?<ProfileCode>L</ProfileCode>', content, re.DOTALL):
+
+        lengths = re.findall(r'<Length>(\d+)</Length>', content)
+        if all(int(length) < 279 for length in lengths):
+
+            tags_to_modify = ['Filename', 'DrawingIdentification', 'PieceIdentification']
+            for tag in tags_to_modify:
+                pattern = re.compile(f"<{tag}>([^<]+)</{tag}>")
+                for match in pattern.finditer(content):
+                    modified_value = match.group(1).replace('0', '')
+                    content = content[:match.start(1)] + modified_value + content[match.end(1):]
+
+            with open(filepath, 'w') as file:
+                file.write(content)
+
+def modify_nc1(filepath):
+    lines = safe_file_read(filepath)
+    if lines is None or len(lines) <= 10:
+        return
+
+    try:
+        length_value = float(lines[10].strip())
+    except ValueError:
+        return
+
+    if length_value < 279:
+        for i in [3, 4]:
+            lines[i] = lines[i].replace('0', '')
 
         with open(filepath, 'w') as file:
             file.writelines(lines)
 
-# Watchdog's handler for file changes
 class FileWatchHandler(FileSystemEventHandler):
-    def on_modified(self, event):
+
+    def on_any_event(self, event):  
         if event.is_directory:
             return
 
-        current_time = time.time()
-        if event.src_path in last_processed and current_time - last_processed[event.src_path] < 5:
-            return
-        
+        if event.src_path in modified_files:
+            return  # Skip already modified files
+
         if event.src_path.endswith('.idstv'):
             modify_idstv(event.src_path)
-            print(f"Modified .idstv file: {event.src_path}")
+            print(f"Processed .idstv file: {event.src_path}")
+            modified_files.add(event.src_path)
         elif event.src_path.endswith('.nc1'):
             modify_nc1(event.src_path)
-            print(f"Modified .nc1 file: {event.src_path}")
-
-        last_processed[event.src_path] = current_time
+            print(f"Processed .nc1 file: {event.src_path}")
+            modified_files.add(event.src_path)
 
 def main():
-    path_to_watch = "C:\\Users\\fab.automation\\Desktop\\Testing angle Processer"
-
-    print(f"Waiting for 15 seconds before starting...")
-    time.sleep(15)
+    path_to_watch = "//Users//coldensheppard//Desktop//VS Code//Github//Watch_folder"
+    print(f"Waiting for 5 seconds before starting...")
+    time.sleep(5)
 
     event_handler = FileWatchHandler()
     observer = Observer()
@@ -84,10 +87,9 @@ def main():
         observer.start()
         print(f"Watching changes in directory: {path_to_watch}")
         while True:
-            time.sleep(1)  # Reduce CPU usage by introducing a slight pause in the infinite loop
+            time.sleep(1)
     except KeyboardInterrupt:
         observer.stop()
-    
     observer.join()
 
 if __name__ == "__main__":
