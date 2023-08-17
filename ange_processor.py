a7a5f6c00d46e53ba360e9f078890bc80c716a0f
shep2363 <colden0889@hotmail.com>
2023-08-17 11:55:11 -0600

    Back to Basics

--- ange_processor.py
+++ ange_processor.py
@@ -1,96 +1,70 @@
-import os
 import re
-import time
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-
-modified_files = set()
-
-def safe_file_read(filepath, retries=5, delay=0.2):
-    """Safely read a file with retry mechanism."""
-    for _ in range(retries):
-        try:
-            with open(filepath, 'r') as file:
-                return file.readlines()
-        except Exception as e:
-            print(f"Error reading file {filepath}. Retrying... Error: {e}")
-            time.sleep(delay)
-    print(f"Failed to read {filepath} after multiple attempts.")
-    return None
+import os
 
 def modify_idstv(filepath):
-    lines = safe_file_read(filepath)
-    if lines is None:
-        return
+    with open(filepath, 'r') as file:
+        lines = file.readlines()
 
-    content = ''.join(lines)
-    if re.search(r'<ProfileType>L</ProfileType>.*?<ProfileCode>L</ProfileCode>', content, re.DOTALL):
+    # Check line 10 for the presence of <ProfileType>L</ProfileType>
+    if len(lines) > 10 and "<ProfileType>L</ProfileType>" in lines[9]:
+        length_pattern = re.compile(r'<Length>(\d+)</Length>')
+        tags_to_modify = ['Filename', 'DrawingIdentification', 'PieceIdentification']
 
-        lengths = re.findall(r'<Length>(\d+)</Length>', content)
-        if all(int(length) < 279 for length in lengths):
-
-            tags_to_modify = ['Filename', 'DrawingIdentification', 'PieceIdentification']
-            for tag in tags_to_modify:
-                pattern = re.compile(f"<{tag}>([^<]+)</{tag}>")
-                for match in pattern.finditer(content):
-                    modified_value = match.group(1).replace('0', '')
-                    content = content[:match.start(1)] + modified_value + content[match.end(1):]
-
-            with open(filepath, 'w') as file:
-                file.write(content)
-
-def modify_nc1(filepath):
-    lines = safe_file_read(filepath)
-    if lines is None or len(lines) <= 10:
-        return
-
-    try:
-        length_value = float(lines[10].strip())
-    except ValueError:
-        return
-
-    if length_value < 279:
-        for i in [3, 4]:
-            lines[i] = lines[i].replace('0', '')
+        for index, line in enumerate(lines):
+            length_match = length_pattern.search(line)
+            if length_match and int(length_match.group(1)) < 279:
+                for tag in tags_to_modify:
+                    start_tag = f"<{tag}>"
+                    end_tag = f"</{tag}>"
+                    if start_tag in line and end_tag in line:
+                        start = line.find(start_tag) + len(start_tag)
+                        end = line.find(end_tag)
+                        modified_value = line[start:end].replace('0', '')
+                        lines[index] = line[:start] + modified_value + line[end:]
 
         with open(filepath, 'w') as file:
             file.writelines(lines)
 
-class FileWatchHandler(FileSystemEventHandler):
+def modify_nc1(filepath):
+    with open(filepath, 'r') as file:
+        lines = file.readlines()
 
-    def on_any_event(self, event):  
-        if event.is_directory:
+    # Check line 11 for the length value
+    if len(lines) > 10:
+        try:
+            length_value = float(lines[10].strip())
+        except ValueError:
+            # If line 11 doesn't contain a numeric value, skip the modifications
             return
+        
+        if length_value < 279:
+            # Modify lines 4 and 5 to remove zeros
+            for i in [3, 4]:  # Lines 4 and 5 have indices 3 and 4
+                lines[i] = lines[i].replace('0', '')
 
-        if event.src_path in modified_files:
-            return  # Skip already modified files
+        with open(filepath, 'w') as file:
+            file.writelines(lines)
 
-        if event.src_path.endswith('.idstv'):
-            modify_idstv(event.src_path)
-            print(f"Processed .idstv file: {event.src_path}")
-            modified_files.add(event.src_path)
-        elif event.src_path.endswith('.nc1'):
-            modify_nc1(event.src_path)
-            print(f"Processed .nc1 file: {event.src_path}")
-            modified_files.add(event.src_path)
+def list_files_in_directory(directory, extension):
+    """Return a list of filenames with the given extension in the specified directory."""
+    with os.scandir(directory) as entries:
+        return [entry.path for entry in entries if entry.is_file() and entry.name.endswith(extension)]
 
 def main():
-    path_to_watch = "//Users//coldensheppard//Desktop//VS Code//Github//Watch_folder"
-    print(f"Waiting for 5 seconds before starting...")
-    time.sleep(5)
+    directory = input("Please enter the directory containing .idstv and .nc1 files: ")
 
-    event_handler = FileWatchHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path=path_to_watch, recursive=False)
+    idstv_files = list_files_in_directory(directory, ".idstv")
+    nc1_files = list_files_in_directory(directory, ".nc1")
 
-    try:
-        observer.start()
-        print(f"Watching changes in directory: {path_to_watch}")
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+    if not idstv_files and not nc1_files:
+        print("No .idstv or .nc1 files found in the specified directory.")
+        return
+
+    for file in idstv_files:
+        modify_idstv(file)
+    
+    for file in nc1_files:
+        modify_nc1(file)
 
 if __name__ == "__main__":
     main()
